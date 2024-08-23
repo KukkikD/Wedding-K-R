ca022d4961695d2d0e4aa3ec87caf45116e65d40
KukkikD <adeezon9@gmail.com>
2024-08-23 14:12:16 -0700

    add new

---
+++ scripts/foot.js
@@ -0,0 +1,5 @@
+// Copyright year
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+//Lasmodify//
+document.getElementById('lastModified').textContent = new Date(document.lastModified).toLocaleDateString();
